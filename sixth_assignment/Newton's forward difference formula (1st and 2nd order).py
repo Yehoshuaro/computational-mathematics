c50194473cb19b6a944813f77fc3fa867cedec39
Yerkhan <231132@astanait.edu.kz>
2025-02-06 23:26:42 +0500

    ewton's forward difference formula (1st and 2nd order)

---
+++ sixth_assignment/Newton's forward difference formula (1st and 2nd order).py
@@ -0,0 +1,26 @@
+def newton_forward_diff1(x, y, h):
+    delta1 = y[1] - y[0]
+    delta2 = y[2] - 2 * y[1] + y[0]
+    delta3 = y[3] - 3 * y[2] + 3 * y[1] - y[0]
+
+    f_prime = (delta1 / h) - (delta2 / (2 * h)) + (delta3 / (3 * h))
+    return f_prime
+
+x_vals = [0, 1, 2, 3, 4]
+y_vals = [1, 2, 4, 8, 16]
+h = x_vals[1] - x_vals[0]
+
+result1 = newton_forward_diff1(x_vals, y_vals, h)
+print("First derivative at x=0:", result1)
+
+def newton_forward_diff2(x, y, h):
+    delta2 = y[2] - 2 * y[1] + y[0]
+    delta3 = y[3] - 3 * y[2] + 3 * y[1] - y[0]
+
+    f_double_prime = (delta2 / h**2) - (delta3 / h**2)
+    return f_double_prime
+
+result2 = newton_forward_diff2(x_vals, y_vals, h)
+print("Second derivative at x=0:", result2)
+
+
